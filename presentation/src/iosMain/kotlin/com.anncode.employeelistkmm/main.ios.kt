a00135c89799fdf10ce2e8a7742bf3489a62c32b
anahi.salgado <anahi.salgado@globant.com>
2023-11-13 11:46:35 -0600

    6 presentation, viewmodel

--- presentation/src/iosMain/kotlin/com.anncode.employeelistkmm/main.ios.kt
+++ presentation/src/iosMain/kotlin/com.anncode.employeelistkmm/main.ios.kt
@@ -1,25 +1,8 @@
 package com.anncode.employeelistkmm
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.window.ComposeUIViewController
 
 fun MainViewController() = ComposeUIViewController {
-    //App()
-    iOSUI()
-
-}
-
-@Composable
-fun iOSUI() {
-    Column {
-        Text("Brett Stiedemann")
-        Text("Deron Wintheiser")
-        Text("Aubrey Schowalter")
-        Text("Mia Dietrich")
-        Text("Orland Cronin")
-        Text("Mathilde Zulauf")
-    }
+    App()
 
 }
